cd18ff493d9d28996887be357223bdae6855055a
Soulake <114913420+Soulaker@users.noreply.github.com>
2026-01-20 19:09:16 +0800

    Update 凯撒加密.py

--- 凯撒加密.py
+++ 凯撒加密.py
@@ -13,29 +13,8 @@ def cae(strings,num):
         else:
             result.append(char)
     return ''.join(result)
-def anticae(strings,num):
-    a=list(string.ascii_uppercase)
-    b=list(string.ascii_lowercase)
-    result = []
-    for char in strings:
-        if char in a:
-            idx = a.index(char)
-            result.append(a[abs(26-num+idx)%26])
-        elif char in b:
-            idx=b.index(char)
-            result.append(b[abs(26-num+idx)%26])
-        else:
-            result.append(char)
-    return ''.join(result)
+print("欢迎使用凯撒加密v1.0")
+strings = str(input("先输入你要加密的字符串:"))
+num = int(input("输入偏移量:"))
+print(cae(strings, num))
 
-co_code =int(input("输入0进入凯撒加密，输入1进入凯撒解密:"))
-if co_code == 0:
-    print("欢迎使用凯撒加密v1.0")
-    strings = str(input("先输入你要加密的字符串:"))
-    num = int(input("输入偏移量:"))
-    print(cae(strings, num))
-if co_code == 1:
-    print("欢迎使用凯撒解密v1.0")
-    strings = str(input("先输入你要解密的字符串:"))
-    num = int(input("输入偏移量:"))
-    print(anticae(strings, num))
